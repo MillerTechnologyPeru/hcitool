820dae2b85c236b151169ba0a75110cecba89aa3
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-05-16 00:48:15 -0700

    Updated `ListDevices`

--- Sources/hcitool/ListDevices.swift
+++ Sources/hcitool/ListDevices.swift
@@ -24,9 +24,19 @@ struct ListDevices: AsyncParsableCommand {
         let list = try HostController.deviceList()
         for device in list {
             let info = try HostController.deviceInformation(for: device.id)
-            print("\(info.name):")
-            print("     " + "Type: \(info.type)" + "  " + "Bus: \(info.busType)")
-            print("     " + "Address: \(info.address)")
+            print("\(info.name):" + "   " + "Type: \(info.type)" + "  " + "Bus: \(info.busType)")
+            print("      " + "Address: \(info.address)")
+            var flags = [String]()
+            if info.flags.contains(.up) == false {
+                flags.append("DOWN")
+            }
+            for flag in HCIDeviceFlag.allCases {
+                guard info.flags.contains(flag) else {
+                    continue
+                }
+                flags.append("\(flag) ".uppercased())
+            }
+            print("      " + flags.reduce("", { $0 + $1 }))
         }
     }
 }
